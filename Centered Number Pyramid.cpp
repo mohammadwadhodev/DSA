92681d8a9dadd9698247231efaaf31b48d18a930
Mohammad <wadho.dev@gmail.com>
2025-03-14 23:36:59 +0500

    Hollow Square Pattern

--- Centered Number Pyramid.cpp
+++ Centered Number Pyramid.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int numbeOfRows = 4;
     int row = 1;
-    
-    while(row <= numbeOfRows){
-        int space  = numbeOfRows - row;
-        while(space){
-             cout << " ";
-             space--;
-            }
-            
-    int left = 1;
-    while(left <=row){
-        cout << left;
-        left++;
-       }
-      
-       int right = row  - 1;
-       while(right){
-        cout << right;
-        right--;
-       }
 
-       cout << endl;
-       row++;
+    while (row <= numbeOfRows)
+    {
+        int space = numbeOfRows - row;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
 
+        int left = 1;
+        while (left <= row)
+        {
+            cout << left;
+            left++;
+        }
+
+        int right = row - 1;
+        while (right)
+        {
+            cout << right;
+            right--;
+        }
+
+        cout << endl;
+        row++;
     }
 }
 
-// OUTPUT  :- 
+// OUTPUT  :-
 
 //      1
 //    1 2 1
 //  1 2 3 2 1
-//1 2 3 4 3 2 1 
+// 1 2 3 4 3 2 1
