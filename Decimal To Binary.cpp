59f407efaf54d1266066195c8f9a787a40189a59
Mohammad <wadho.dev@gmail.com>
2025-03-16 01:15:16 +0500

    Decimal to Binary

---
+++ Decimal To Binary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int decimal = 0;
+    cout << "Enter any decimal number" << endl;
+    cin >> decimal;
+    
+    
+    string binary = "";
+    while(decimal >= 1){
+        binary = to_string(decimal % 2) + binary;
+        decimal /= 2;
+        
+    }
+
+    cout << binary;
+    
+
+
+
+
+
+}
