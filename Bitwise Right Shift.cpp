f5ef2dbed37abf86e118e561677de79976b488ac
Mohammad <wadho.dev@gmail.com>
2025-03-17 19:44:08 +0500

    Bitwise Right Shift

---
+++ Bitwise Right Shift.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    
+    cout << "Output : " << (0b1010 >> 2) << endl;
+
+
+}
---
+++ Bitwise Xor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int value = 0;
+    int value2 = 0;
+    cout <<  "Enter value 1: " << endl;
+    cin >> value;
+    cout <<  "Enter value 2: " << endl;
+    cin >> value2;
+    cout << endl;
+    
+    cout << "Output : " << (value ^ value2) << endl;
+
+
+}
