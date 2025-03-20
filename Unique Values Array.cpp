17ff466637767942b7bd45b2b831e556e933105c
Mohammad <wadho.dev@gmail.com>
2025-03-21 04:43:33 +0500

    Find unique values in array

---
+++ Unique Values Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+
+void findUniques(int arr[], int length){
+
+    string uniquValues = "";
+
+    for(int i = 0; i< length; i++){
+        int isUnique = 1;
+        for(int j = 0; j < length; j++){
+            if(i != j && arr[i] == arr[j]){
+                isUnique = 0;
+                break; 
+            }
+        }
+        
+        // Print if the element is unique
+        if (isUnique) {
+            std::cout << arr[i] << " ";
+        }
+    }
+    std::cout << endl;
+
+}
+
+int main(){
+
+    int arr[9] = {1,2,3,4,9,4,3,2,7};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    findUniques(arr,length);
+
+}
