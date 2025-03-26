e29be734603fae3698c7d6eba366f2c0234aaa45
Mohammad <wadho.dev@gmail.com>
2025-03-27 03:19:56 +0500

    update

--- sub_array.cpp
+++ sub_array.cpp
@@ -6,16 +6,15 @@ using namespace std;
 int main() {
     vector<int> vec = {1, 2, 3, 4, 5};
 
-    // Loop to generate subarrays
+
     for (int n = 0; n < vec.size(); n++) {
         for (int i = n; i < vec.size(); i++) {
-            // Print subarray from index n to i
             for (int k = n; k <= i; k++) {
                 cout << vec[k];
             }
-            cout << "  ";  // Space between different subarrays
+            cout << "  ";
         }
-        cout << endl;  // New line after each starting index
+        cout << endl; 
     }
 
     return 0;
