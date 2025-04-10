98efbb766bf44e6497732caa3ea05b62273e2bec
Mohammad <wadho.dev@gmail.com>
2025-04-10 05:16:21 +0500

    Majority Element By Optimized approach

---
+++ Majority_element_by_optimized_approach.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+int main(){
+    
+    vector<int> nums = {1,2,2,1,1,1};
+
+    sort(nums.begin(),nums.end());
+    
+    int freq = 1, ans = nums[0];
+
+    for(int i = 1; i< nums.size(); i++){
+        if(nums[i] == nums[i - 1]){
+            freq++;
+        }else{
+            freq = 1;
+            ans = nums[i];
+        }
+        
+        if(ans > nums.size() / 2){
+            cout << "Majority Element " << ans << endl;
+            break;
+    }
+
+}   
+
+
+// I thinks this is not working for case 3 
+
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// Example 3:
+
+// Input: nums = [1,2,2,1,1,1]
+// Output: 1
+
+
+
+// g++ -std=c++11 Majority_element_by_optimized_approach.cpp -o MajorityElementByOptimize
+// ./MajorityElementByOptimize
+
+}
